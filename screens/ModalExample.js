9bc8af2d3daab4a5934d307f9b458bb8ede39493
Ahmad Raza <razatastic@gmail.com>
2020-05-28 02:13:42 -0400

    Convert to async modules

--- screens/ModalExample.js
+++ screens/ModalExample.js
@@ -8,7 +8,7 @@ import {
   Image,
   ScrollView
 } from 'react-native'
-import { Card, ListItem, Button, Icon } from 'react-native-elements'
+import { Card, Button } from 'react-native-elements'
 import cio from 'cheerio-without-node-native'
 
 class ModalExample extends Component {
@@ -22,59 +22,60 @@ class ModalExample extends Component {
     this.setState({ modalVisible: visible })
   }
 
-  submitPIDtoPPG(pid) {
-    return fetch('https://www.poppriceguide.com/guide/trackerupdates.php', {
-      method: 'POST',
-      headers: {
-        'user-agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36',
-        origin: 'https://www.poppriceguide.com',
-        'x-requested-with': 'XMLHttpRequest',
-        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        accept: '*/*',
-        'cache-control': 'no-cache',
-        'accept-encoding': 'gzip, deflate, br',
-        'accept-language': 'en-US,en;q=0.9'
-      },
-      body: 'cType=addcollitem&cUserID='
-        .concat(this.state.ppgUserID)
-        .concat('&cItemID=')
-        .concat(pid)
-        .concat(
-          '&cName=&cReplace=N&cPurchaseDate=&cPurchasePrice=0.00&cPurchasedFrom=&cPkgCondition=2&cNotes='
-        )
-    })
-      .then((response) => {
-        console.log('Submitted')
-      })
-      .catch((error) => {
-        console.error(error)
-      })
+  async submitPIDtoPPG(pid) {
+    try {
+      const response = await fetch(
+        'https://www.poppriceguide.com/guide/trackerupdates.php',
+        {
+          method: 'POST',
+          headers: {
+            'user-agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36',
+            origin: 'https://www.poppriceguide.com',
+            'x-requested-with': 'XMLHttpRequest',
+            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            accept: '*/*',
+            'cache-control': 'no-cache',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'en-US,en;q=0.9'
+          },
+          body: 'cType=addcollitem&cUserID='
+            .concat(this.state.ppgUserID)
+            .concat('&cItemID=')
+            .concat(pid)
+            .concat(
+              '&cName=&cReplace=N&cPurchaseDate=&cPurchasePrice=0.00&cPurchasedFrom=&cPkgCondition=2&cNotes='
+            )
+        }
+      )
+      console.log('Submitted')
+    } catch (error) {
+      console.error(error)
+    }
   }
 
-  fetchfromHobby(hobbyID, productName) {
+  async fetchfromHobby(hobbyID, productName) {
     const hobbyUrl = 'https://www.hobbydb.com/api/catalog_items/'
       .concat(hobbyID)
       .concat('/curator_price_guide')
-    return fetch(hobbyUrl, {
-      method: 'GET',
-      headers: {
-        'user-agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36',
-        'cache-control': 'no-cache',
-        'accept-encoding': 'gzip, deflate, br',
-        'accept-language': 'en-US'
-      }
-    })
-      .then((response) => response.json())
-      .then((responseJson) => {
-        console.log('Found in HobbyDB: ' + responseJson.data.attributes.name)
-        this.fetchFromPPG(responseJson.data.attributes.name)
+    try {
+      const response = await fetch(hobbyUrl, {
+        method: 'GET',
+        headers: {
+          'user-agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36',
+          'cache-control': 'no-cache',
+          'accept-encoding': 'gzip, deflate, br',
+          'accept-language': 'en-US'
+        }
       })
-      .catch((error) => {
-        this.fetchFromPPG(productName)
-        console.error(error)
-      })
+      const responseJson = await response.json()
+      console.log('Found in HobbyDB: ' + responseJson.data.attributes.name)
+      this.fetchFromPPG(responseJson.data.attributes.name)
+    } catch (error) {
+      this.fetchFromPPG(productName)
+      console.error(error)
+    }
   }
 
   async fetchFromPPG(productName) {
